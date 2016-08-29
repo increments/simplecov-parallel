0c6a83bfd7f43c90fff6d34e3c24184cc0f1d395
Yuji Nakayama <nkymyj@gmail.com>
2016-08-29 14:53:04 +0900

    bundle gem simplecov-parallel

---
+++ lib/simplecov/parallel.rb
@@ -0,0 +1,7 @@
+require "simplecov/parallel/version"
+
+module Simplecov
+  module Parallel
+    # Your code goes here...
+  end
+end
---
+++ lib/simplecov/parallel/version.rb
@@ -0,0 +1,5 @@
+module Simplecov
+  module Parallel
+    VERSION = "0.1.0"
+  end
+end
