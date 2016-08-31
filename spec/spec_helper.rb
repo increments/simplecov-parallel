0a12cee8baf4a2aae10caa0e602932b9a63ea7eb
Yuji Nakayama <nkymyj@gmail.com>
2016-08-31 15:07:41 +0900

    Add a comment about the impossibility of self hosting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -97,3 +97,6 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 end
+
+# We cannot use simplecov-parallel itself for simplecov-parallel's specs
+# because we need to load simplecov-parallel _before_ running the specs...
